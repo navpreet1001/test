c6f96213be79f5e4c98aadae81b91f5559d04a16
Navpreet <navpreetgagar@gmail.com>
2017-12-19 20:04:48 -0400

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
+
+const appRoutes : Routes =[
+  {path: '', component: HomeComponent, pathMatch:'full'},
+  {path: 'users', component: UsersComponent, pathMatch:'full'},
+  {path: 'users/:id/:name', component: UserComponent, pathMatch:'full'},
+  {path: 'servers', component: ServersComponent, pathMatch:'full'},
+  {path: 'servers/:id/edit', component: EditServerComponent, pathMatch:'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [ServersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
